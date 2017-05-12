2239263ba8198edf8d5a5970e6b94320dec77c32
Nathan Grey <Nathan@NathanGrey.com>
2017-05-11 20:49:24 -0700

    Update New.cpp

--- New.cpp
+++ New.cpp
@@ -1,106 +1,105 @@
 #include <iostream> // Starts the preprocessor command including the contents of iostream
 using namespace std;   // This eliminates the need to write std::cout so it can just be cout
 
-int BoardSize = 0;
-char** GameBoard = nullptr;
-char Player;
+// Declaration of variables
+int BoardSize = 0; // Sets the board size to 0 until defined by the user
+char** GameBoard = nullptr; // Sets pointer to nothing since size varies
+char Player; // Player object
 
-enum SpaceCases {
-        Player_one = 'X',
-        Player_two = 'O',
+enum SpaceCases { // Sets the spaces to change to X's, O's or *
+        PlayerOne = 'X',
+        PlayerTwo = 'O',
         Neutral    = '*'
     };
 
- 
-void PrintBoard(){
+void PrintBoard(){ // Function to print the board spaxes
     for (int i = 0; i < BoardSize; i++){
         for (int j = 0; j < BoardSize; j++){
-            cout<< GameBoard[i][j]<< "   ";
+            cout<< GameBoard[i][j] << "   ";
         }
         cout<< "\n\n";
     }
 }
-bool CheckIfGameOver() // Function to check if the game is over
+
+bool CheckIfGameOver() // Function to check for the winner
 {
     for (int i = 0; i < BoardSize; ++i)
     {
         for (int j = 0; j < BoardSize; ++j)
         {
-            if (GameBoard[i][j] != '*')
+            if (GameBoard[i][j] != '*') // If a space is not neutral the checking begins
             {
                 if ((i - 1) >= 0 && (j - 1) >= 0 && (i + 1) < BoardSize && (j + 1) < BoardSize)
                 {
-                    // Diagonal down
+                    // Checks the diagonal down
                     if (GameBoard[i - 1][j - 1] == GameBoard[i][j] && GameBoard[i + 1][j + 1] == GameBoard[i][j])
                         return true;
-                    // Diagonal up
+                    // Checks the diagonal up
                     if (GameBoard[i + 1][j - 1] == GameBoard[i][j] && GameBoard[i - 1][j + 1] == GameBoard[i][j])
                         return true;
                 }
                 if ((i - 1) >= 0 && (i + 1) < BoardSize)
                 {
-                    // Vetical
+                    // Checks the columns
                     if (GameBoard[i - 1][j] == GameBoard[i][j] && GameBoard[i + 1][j] == GameBoard[i][j])
                         return true;
                 }
                 if ((j - 1) >= 0 && (j + 1) < BoardSize)
                 {
-                    // Horizontal
+                    // Checks the rows
                     if (GameBoard[i][j - 1] == GameBoard[i][j] && GameBoard[i][j + 1] == GameBoard[i][j])
                         return true;
                 }
             }
         }
     }
-
     return false;
 }
 
+void PlayerOneMove(){ // Player One's movement
 
-void PlayerOneMove(){
+    int i, j;
+    bool Valid = false;
 
-    int i;
-    int j;
-    bool valid = false;
-
-    while (!valid){
-        cout<< "Player one decide position\n";
-        cin>> i;
-        cin>> j;
+    while (!Valid){
+        cout << "Player One | Choose a space." << endl;
+        cin >> i;
+        cin >> j;
         if (GameBoard[i-1][j-1] == '*'){
-            valid = true;
+            Valid = true;
         }
         else{
-            cout<< "This position is already taken\n";
+            cout << "This space is already taken." << endl;
         }
 
     }
-    cout<< "\n";
-    GameBoard[i-1][j-1] = static_cast<char>(Player);
+    cout << endl;
+    GameBoard[i-1][j-1] = static_cast<char>(Player); 
 }
-void PlayerTwoMove(){
 
-    int i;
-    int j;
-    bool valid = false;
+void PlayerTwoMove(){ // Player Two's movement
 
-    while (!valid){
-        cout<< "Player two decide position\n";
-        cin>> i;
-        cin>> j;
+    int i, j;
+    bool Valid = false;
+
+    while (!Valid){
+        cout << "Player Two | Choose a space." << endl;
+        cin >> i;
+        cin >> j;
         if (GameBoard[i-1][j-1] == '*'){
-            valid = true;
+            Valid = true;
         }
         else{
-            cout<< "This position is already taken\n";
+            cout << "This space is already taken." << endl;
         }
 
                     
     }
-    cout<< "\n";
+    cout<< endl;
     GameBoard[i-1][j-1] = static_cast<char>(Player);
 }
-bool isBoardFull()
+
+bool BoardFull() // Checks if the board is filled
 {
     for (int i = 0; i < BoardSize; ++i)
     {
@@ -115,7 +114,7 @@ bool isBoardFull()
     return true;
 }
 
-void deleteBoard()
+void DeleteBoard() // Deletes the board that was dynamically allocated
 {
     for (int i = 0; i < BoardSize; ++i)
     {
@@ -124,104 +123,92 @@ void deleteBoard()
     delete[] GameBoard;
 }
 
-
-int main()
+int main() // Main program
 {
-        int Order = 0;
-        bool GameOver = false;
+    bool GameOver = false;
            
-        do
-        {
-        cout << "Welcome to Mega Tic Tac Toe!" << endl;
-        cout << "Choose your board size:" << endl;
+    do
+    {
+        cout << "---------------------------------------------------------" << endl << endl;
+        cout << "             Welcome to Mega Tic Tac Toe!" << endl << endl;
+        cout << "---------------------------------------------------------" << endl << endl;
+        cout << "Choose a board size that is greater than or equal to 3." << endl << endl;
 
         cin >> BoardSize; // Declares the board's size 
         
-        } while (BoardSize < 3);
-// Computes the board size dynamically
-    GameBoard = new char*[BoardSize];
-    for (int i = 0; i < BoardSize; ++i)
-    {
-        GameBoard[i] = new char[BoardSize];
-    }
-
-
-
-  for (int i = 0; i < BoardSize; i++)
-     { 
-            for (int j = 0; j < BoardSize; j++)
-            {
-                GameBoard[i][j] = static_cast<char>(Neutral);
-            }
+    } while (BoardSize < 3);
+        // Allocates the board size dynamically
+        GameBoard = new char*[BoardSize];
+        for (int i = 0; i < BoardSize; ++i)
+        {
+            GameBoard[i] = new char[BoardSize];
         }
+        // Initializes the board
+          for (int i = 0; i < BoardSize; i++)
+             { 
+                    for (int j = 0; j < BoardSize; j++)
+                    {
+                        GameBoard[i][j] = static_cast<char>(Neutral);
+                    }
+                }
 
+        cout << endl << "You board will be " << BoardSize << " x " << BoardSize << endl << endl;
 
-        cout << endl << "You board will be (" << BoardSize << ", " << BoardSize << ")" << endl;
+        PrintBoard(); // Prints the board size
 
-        PrintBoard();
-            // Loop to keep playing until game over
-    while (!GameOver){
-
-        if (Order % 2 == 0){
-            Player = static_cast<char>(Player_one);
+    while (!GameOver) // Loop to keep playing until game over
+    { 
+            Player = static_cast<char>(PlayerOne);
             PlayerOneMove();
             PrintBoard();
             GameOver = CheckIfGameOver();
-           if (!GameOver && isBoardFull())
+            if (!GameOver && BoardFull())
                     {
-                        cout << "ITS A TIE" << endl;
+                        cout << "You both tied!" << endl;
                         break;
                         return 0;
                     }
                     else if (GameOver)
                     {
-                        if (PlayerOneMove)
+                        if (GameOver && Player == PlayerOne)
                         {
-                            cout << "Player X WINS!!!" << endl;
+                            cout << "Player One wins!" << endl;
                             return 0;
                         }
                         else
                         {
-                            cout << "Player O WINS!!!" << endl;
+                            cout << "Player Two wins!" << endl;
                             return 0;
                         }
                     }
 
-        }
-
-        else{
-            Player = static_cast<char>(Player_two);
+            Player = static_cast<char>(PlayerTwo);
             PlayerTwoMove();
             PrintBoard();
             GameOver = CheckIfGameOver();
             
-                if (!GameOver && isBoardFull())
+            if (!GameOver && BoardFull())
                     {
-                        cout << "ITS A TIE" << endl;
+                        cout << "You both tied!" << endl;
                         break;
                         return 0;
                     }
                     else if (GameOver)
                     {
-                        if (PlayerOneMove)
+                        if (GameOver && Player == PlayerOne)
                         {
-                            cout << "Player X WINS!!!" << endl;
+                            cout << "Player One wins!" << endl;
                             return 0;
                         }
                         else
                         {
-                            cout << "Player O WINS!!!" << endl;
+                            cout << "Player Two wins!" << endl;
                             return 0;
                         }
                     }
-        }
-        Order++;
-    }
-        PlayerOneMove();
-        PlayerTwoMove();
-        CheckIfGameOver();
 
-        deleteBoard();
+    }
+        DeleteBoard(); // Manages the memory by deleting the board
         
     system("pause");
     return 0;
